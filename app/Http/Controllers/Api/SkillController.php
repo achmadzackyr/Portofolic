ce83e2e0e7c634bf5c3868397aa0d815a48c2925
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-06-04 11:33:06 +0700

    Get & Add Skill Endpoint

---
+++ app/Http/Controllers/Api/SkillController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SkillController extends Controller
+{
+    public function index()
+    {
+        $skills = Skill::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new SkillResource(true, 'Skill Lists', $skills);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'skill_name' => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $skill = Skill::create([
+            'skill_name' => $request->skill_name,
+        ]);
+
+        //return response
+        return new SkillResource(true, 'Skill successfully added!', $skill);
+    }
+}
